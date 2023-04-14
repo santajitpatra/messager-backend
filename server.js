a2773bafdda44202ae8b5be37878c9dd47c45865
SantajitPatra <business.santajitpatra@gmail.com>
2023-04-14 23:53:08 +0530

    init: create

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  
+
+  try {
+  const r = await axios.put(
+    'https://api.chatengine.io/users/',
+    {username: username, secret: username, first_name: username},
+    {headers: {"private-key": "8c63dbce-80a7-455a-890b-9368d16e1dcb "}}
+  )
+  return res.status(r.status).json(r.data)
+
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+
+  }
+  
+  // return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
